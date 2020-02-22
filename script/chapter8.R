9f204beecbc1249a086a3971b9f4f50ccad20321
knecht <ldj910505@naver.com>
2020-02-22 16:59:44 +0900

    20200222 chapter 9 commit

--- script/chapter8.R
+++ script/chapter8.R
@@ -1,5 +1,204 @@
 # chapter8
 # ###############################################
-# 작성자 :
-# 작성일 :
+# 작성자 : 이동준
+# 작성일 : 20200222
 # ###############################################
+
+# 01 선형 회귀의 기본 가정
+# 종속 변수와 독립 변수 간에 선형성이 성립한다
+# 독립변수는 고정된 값이다.
+# 오차는 정규분포를 따른다.
+# 독립 변수 간에는 다중 공선성이 적어야 한다. 
+
+# 02 단순 선형 회귀
+# 종속 변수 y1을 하나의 독립변수 x1으로 설명한다.
+# 두 개 이상의 독립변수로 설명하는 경우는 중선형 회귀라한다.
+
+# 모델 생성
+data(cars)
+head(cars)
+(m <- lm(dist ~ speed , cars))
+# dist = -17.359 + 3.932 * speed + ε
+
+# 선형 회귀 결과 추출
+# 회귀 계수 : coef(model)
+coef(m)
+# 적합값 : car 데이터의 각 speed 값에 대한 모델에 의해 예측된 값은 fitted()로 구할 수 있다.
+fitted(m)[1:4]
+# 잔차 : residuals(model)
+residuals(m)[1:4]
+# 적합된 값과 잔차의 합은 실제 데이터 값과 같다
+fitted(m)[1:4] + residuals(m)[1:4]
+cars$dist[1:4]
+# 회귀 계수의 신뢰 구간(confint(model))
+confint(m)
+# 잔차 제곱합
+deviance(m)
+
+# 예측과 신뢰 구간
+(m <- lm(dist ~ speed, data = cars))
+predict(m, newdata = data.frame(speed=3))
+answer <- coef(m)
+answer[1] + answer[2] * 3
+
+# 회귀 계수의 신뢰 구간을 구하기 위해 type = "confidence"를 지정하면
+# 제동거리의 평균 신뢰 구간을 구할 수 있다.
+predict(m, newdata = data.frame(speed=c(3)), interval = "confidence")
+# 오차를 포함한 예측 구간을 구할 수도 있다.
+predict(m, newdata = data.frame(speed=c(3)), interval = "prediction")
+
+# 모델 평가
+summary(m)
+# spped 계수 3.9324rk 0이 아니라고 결론을 내릴 수 있다.
+# F 통계량은
+# MSR/MSE의 비율을 F 분포를 사용해 검정한 것이며, 
+# 이 값은 dist = β0 + ε 의 축소 모델과 dist = β0 + β1 * speed + ε의 완전 모델 간에
+# 잔차제곱합이 얼마나 유의하게 다른지 보는 방식과 같다.
+
+# 분산 분석 및 모델 간의 비교
+anova(m)
+
+# 원래 사용 모델
+(full <- lm(dist ~ speed , data = cars))
+# dist를 상숫값으로 예측한 경우
+(reduced <- lm(dist ~ 1 , data = cars))
+
+# 다음은 anova()로 full과 reduced를 비교한 결과다
+anova(reduced, full)
+# reduced 모델과 full 모델 간에는 유의한 차이가 있다고 결론을 내린다.
+# speed 열이 유의미한 설명 변수임을 뜻한다.
+
+plot(m)
+# plot1 기울기 0을 보이는 것이 이상적이다.
+# 선형 회귀에서 오차는 평균이 0이고 분산이 일정한 정규분포를 가정
+# plot2 잔차 정규분포 확인
+# plot3 0에서 멀리 떨어진 값은 이상치일 가능성이 있다.
+# plot4 쿡의 거리 확인(회귀선의 모양에 영향을 끼치는 점들 위치)
+
+plot(m, which = c(4, 6))
+# 순서별 쿡의 거리
+# 레버리지와 쿡의 거리
+
+# 회귀 직선의 시각화
+plot(cars$speed, cars$dist)
+# 선형회귀 라인 그리기
+abline(coef(m))
+summary(cars$speed)
+# 신뢰구간을 구한다.
+predict(m,
+        newdata = data.frame(speed = seq(4.0, 25.0, 0.2)),
+        interval = "confidence")
+speed <- seq(min(cars$speed), max(cars$speed), 0.1)
+speed
+ys <- predict(m, data.frame(speed = speed), interval = "confidence")
+matplot(speed, ys, type = 'n')
+matlines(speed, ys , lty = c(1, 2, 2), col=1)
+
+# 03 중선형 회귀
+(m <- lm(data = iris, Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width))
+summary(m)
+
+(m <- lm(Sepal.Length ~ . , data = iris))
+# 범주형 변수 Species를 다음과 같이 2개의 가변수를 사용해 표현했기 때문이다.
+summary(m)
+
+model.matrix(m)[c(1,51,101),]
+
+anova(m)
+
+# 중선형 회귀 모델의 시각화
+with(iris, plot(Sepal.Width, Sepal.Length,
+                cex = .7,#점의 크기
+                pch = as.numeric(Species)))# 점의 형태
+as.numeric(iris$Species)
+
+m <- lm(data = iris, Sepal.Length ~ Sepal.Width + Species)
+coef(m)
+
+abline(2.25 , 0.80, lty = 1)
+abline(2.25 + 1.45, 0.80, lty = 2)
+abline(2.25 + 1.94, 0.80, lty = 3)
+legend("topright", levels(iris$Species), pch = 1:3 , bg = "white")
+
+# 표현식을 위한 I()의 사용
+x <- 1:1000
+y <- x^2 + 3 * x + 5 + rnorm(1000)
+lm(y ~ I(x^2) + x)
+lm(y ~ x^2 )
+
+x1 <- 1:1000
+x2 <- 3 * x1
+y <- 3 * (x1 + x2) + rnorm(1000)
+lm(y ~ I (x1 + x2))
+lm(y ~ x1 + x2)
+
+# 변수의 변환
+x <- 101:200
+y <- exp(3 * x + rnorm(100))
+lm(log(y) ~ x)
+
+x <- 101:200
+y <- log(x) + rnorm(100)
+lm(y ~ log(x))
+
+# 상호 작용
+data("Orange")
+Orange
+with(Orange,
+     plot(Tree, circumference, xlab = "tree", ylab = "circumference"))
+with(Orange,
+     interaction.plot(age, Tree, circumference))
+# 나무의 수령이 높아짐에 따라 둘레가 길어지는 추세가 관찰되었다.
+
+# 
+Orange[, "fTree"] <- factor(Orange[, "Tree"], ordered = F)
+m <- lm(circumference ~ fTree * age , data = Orange)
+anova(m)
+head(model.matrix(m))
+mm <- model.matrix(m)
+mm[, grep("age", colnames(mm))]
+
+# 04 이상치
+data("Orange")
+m <- lm(circumference ~ age + I(age^2), data= Orange)
+rstudent(m)
+library("car")
+outlierTest(m)
+data("Orange")
+Orange <- rbind(Orange,
+                data.frame(Tree=as.factor(c(6,6,6)),
+                           age = c(118,484,664),
+                           circumference = c(177,50,30)))
+tail(Orange)
+m <- lm(circumference ~ age + I(age^2), data= Orange)
+outlierTest(m)
+# 36번 데이터의 문제
+
+# 05 변수 선택
+# 변수 선택 방법
+# 1. 전진 선택법
+# 2. 변수 소거법
+# 3. 단계적 방법(stepwise)
+
+# install.packages("mlbench")
+library("mlbench")
+data("BostonHousing")
+head(BostonHousing)
+m <- lm(medv ~ ., data=BostonHousing)
+m2 <- step(m, direction = "both")
+formula(m2)
+step(m, direction = "both")
+lm(formula = medv ~ crim + zn + chas + nox + rm + dis + rad + 
+     tax + ptratio + b + lstat, data = BostonHousing)
+
+# 모든 경우에 대한 비교
+install.packages("leaps")
+library("leaps")
+library(mlbench)
+data("BostonHousing")
+m <- regsubsets(medv ~ ., data=BostonHousing)
+summary(m)
+summary(m)$bic
+summary(m)$adjr2
+plot(m, scale="adjr2")
+plot(m, scale="bic")
--- script/chapter9.R
+++ script/chapter9.R
@@ -1,5 +1,205 @@
 # chapter9
 # ###############################################
-# 작성자 :
-# 작성일 :
+# 작성자 : 이동준
+# 작성일 : 20200222
 # ###############################################
+rm(list = ls())
+# 01 데이터 탐색
+str(mtcars)
+# install.packages("Hmisc")
+library("Hmisc")
+describe(mtcars)
+summary(mtcars)
+# fun mean
+summary(mpg ~ cyl + hp, data = mtcars)
+# fun var
+summary(mpg ~ cyl + hp, data = mtcars, fun = var)
+summary(cyl ~ mpg + hp, data = mtcars, method = "reverse")
+# method = "cross"는 rhs에 나열한 변수의 조합으로 lhs를 요약할 때 사용한다.
+summary(mpg ~ cyl + hp, data = mtcars, method = "cross")
+
+plot(iris)
+plot(iris$Sepal.Length)
+plot(iris$Species)
+plot(Species ~ Sepal.Length , data = iris)
+with(iris, {
+  plot(Sepal.Length, Sepal.Width, pch = as.numeric(Species))
+  legend("topright", legend = levels(iris$Species), pch = 1:3)
+})
+
+# install.packages("caret", dependencies = T)
+library("caret")
+featurePlot(iris[, 1:4], iris$Species, "ellipse")
+
+# 02 w전처리
+# 모델링에 알맞은 형태로 데이터를 처리하는 것이 preprocessing
+# feature scaling
+# scale(center T : 모든 데이터에서 평균을 뺀다
+# scale = T : 모든 데이터를 전체 데이터의 표준편차로 나눈다)
+cbind(as.data.frame(scale(iris[1:4])), iris$Species)
+
+# pca principal component analysis
+# 차원 축소 기법 중 하나다
+# 변수들을 주성분이라 부르는 선형적 상관관계가 없는 다른 변수들로 재표현한다.
+# 주성분들은 원 데이터의 분산을 최대한 보존하는 방법으로 구한다.
+x <- 1:10
+y <- x + runif(10, min = -0.5, max = 0.5)
+z <- x + y + runif(10, min=-10, max=.10)
+(data <- data.frame(x, y, z))
+head(data)
+pr <- princomp(data)
+summary(pr)
+pr
+pr$scores[, 1:2]
+
+# 원 핫 인코딩
+(all <- factor(c(paste0(LETTERS, "0"), paste0(LETTERS, "1"))))
+(data <- data.frame(lvl = all, value=rnorm(length(all))))
+install.packages("randomForest")
+library("randomForest")
+m <- randomForest(value ~ lvl , data = data)
+(x <- data.frame(lvl = factor(c("A", "B", "A", "A", "C")),
+                 value = c(1,3,2,4,5)))
+model.matrix(~lvl, data= x)[,-1]
+
+# 결측치의 처리
+iris_na <- iris
+iris_na[c(10, 20, 25, 40, 32), 3] <- NA
+iris_na[c(33, 100, 123), 1] <- NA
+iris_na[!complete.cases(iris_na),]
+iris_na[is.na(iris_na$Sepal.Length),]
+
+install.packages("DMwR")
+library("DMwR")
+iris_na[!complete.cases(iris_na),]
+which(!complete.cases(iris_na))
+centralImputation(iris_na[1:4])[which(!complete.cases(iris_na)),]
+knnImputation(iris_na[1:4])[which(!complete.cases(iris_na)),]
+
+# 변수 선택
+# 0에 가까운 분산
+# 분산이 0에 가까운 변수를 쳐내야 한다.
+library("caret")
+library("mlbench")
+data("Soybean")
+model <- nearZeroVar(Soybean, saveMetrics = T)
+model$nzv
+nearZeroVar(Soybean)
+(mySoybean <- Soybean[, -nearZeroVar(Soybean)])
+head(mySoybean)
+
+# 상관 계수
+# 상관 계수가 높은 변수가 여럿 존재하면 파라미터 수가 불필요하게 증가하여 차원 저주에 빠질 우려가 있다.
+library("mlbench")
+library("caret")
+data("Vehicle")
+correl <- findCorrelation(cor(subset(Vehicle, select=-c(Class))))
+cor(subset(Vehicle, select=-c(Class)))[correl, correl]
+
+# install.packages("FSelector")
+library("FSelector")
+library("mlbench")
+data("Ozone")
+head(Ozone)
+(v <- linear.correlation(V4 ~ .,
+                         data=subset(Ozone, select=-c(V1, V2, V3))))
+
+# 
+library("FSelector")
+library("mlbench")
+data("Vehicle")
+(cs <- chi.squared(Class ~ ., data=Vehicle))
+cutoff.k(cs, 3)
+
+# 변수의 중요도 평가
+library("mlbench")
+library("rpart")
+library("caret")
+data("BreastCancer")
+head(BreastCancer)
+m <- rpart(Class ~ ., data=BreastCancer)
+# 변수의 중요도 평가
+varImp(m)
+
+# 03 모델 평가 방법
+# 평가 메트릭
+predicted <- as.factor(c(1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1))
+actual <- as.factor(c(1, 0, 0, 1, 1, 0, 1, 1 ,0, 1, 1, 1))
+
+xtabs ( ~ predicted + actual)
+NROW(actual)
+nrow(actual)
+sum(predicted == actual)/NROW(actual)
+
+library("caret")
+confusionMatrix(data = predicted, reference = actual)
+
+# roc 커브
+# roc 커브는 fp rate 대비 tp rate의 변화를 뜻한다
+# 이러한 이유로 ROC 커브를 그린 뒤 그 아래의 면적을 
+# auc area under the curve 라 하여 모델 간의 비교에 사용한다.
+library("ROCR")
+set.seed(137)
+probs <- runif(100)
+labels <- as.factor(ifelse(probs > .5 & runif(100) < .4 , "A", "B"))
+library("ROCR")
+pred <- prediction(probs, labels)
+plot(performance(pred, "tpr", "fpr"))
+plot(performance(pred, "acc", "cutoff"))
+performance(pred, "auc")
+
+# 교차 검증
+# 훈련 데이터과 테스트 데이터를 분리하여 모델을 만드는 방법 중 가장
+# 자주 사용하는 기법으로 데이터를 다수의 조각으로 나누너 훈련과 테스트를 반복하는 기법
+# 교차 검증은 훈련 데이터와 검증 데이터로 나누어 모델링 및 평가하는 작업을 K회 반복하는 것으로
+# 이를 K겹 교차 검증이라 한다.
+# 최종 성능은 이들의 산술 평균으로 정할 수 있다.
+# 과적합
+# 주어진데이터로부터 보장되는 것 이상으로 모델을 만들 때 발생한다
+
+install.packages("cvTools")
+library("cvTools")
+cvFolds(10, K = 5, type = "random")
+# 연속된 데이터를 검증으로 사용
+cvFolds(10, K = 5, type = "consecutive")
+# 연속된 데이터를 차례로 서로 다른 K개의 검증 데이터로 사용
+cvFolds(10, K = 5, type = "interleaved")
+
+set.seed(719)
+(cv<- cvFolds(NROW(iris), K = 10, R = 3))
+
+head(cv$which, 20)
+head(cv$subsets)
+
+(validation_idx <- cv$subsets[which(cv$which == 1), 1])
+train <- iris[-validation_idx,]
+validation <- iris[validation_idx, ]
+
+library(foreach)
+set.seed(719)
+R = 3
+K = 10
+cv <- cvFolds(NROW(iris), K=K, R=R)
+foreach(r=1:R) %do% {
+  foreach(k=1:K, .combine = c) %do%{
+    validation_idx <- cv$subsets[which(cv$which == k), r]
+    train <- iris[-validation_idx,]
+    validation <- iris[validation_idx,]
+    # 데이터 전처리
+    
+    # 모델 훈련
+    
+    # 예측
+    
+    # 성능 평가
+    return(0)
+  }
+}
+
+# 
+library(caret)
+(parts <- createDataPartition(iris$Species, p = 0.8))
+table(iris[parts$Resample1, "Species"])
+table(iris[-parts$Resample1, "Species"])
+createFolds(iris$Species, k = 10)
+createMultiFolds(iris$Species, k = 10, times = 3)
